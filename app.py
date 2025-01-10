9d69d0233baf202a98fdda37d88fbce9c8125aff
N Akash Selvadoss <140231948+akashdoss@users.noreply.github.com>
2025-01-10 22:55:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -162,4 +162,5 @@ def check_availability():
 
 if __name__ == '__main__':
     app.config['SECRET_KEY'] = 'your-secret-key-here'  
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=8080)  
+
